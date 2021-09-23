7d5b9fd9761b147f50c56fd85b68e33254dc4ad4
Mrityunjay Singh <Mrityunjay.singh119@gmail.com>
2021-09-23 18:52:06 +0530

    Currency Conveter

---
+++ CurrencyConvert/tempCodeRunnerFile.py
@@ -0,0 +1,5 @@
+amount = int(input("Enter Amount:\n"))
+# print("Enter the name of currency you want to convert this amount to?\nAvailable Options:\n")
+# [print(item) for item in currencyDict.keys()]
+# currency = input("Please enter one of these value:\n")
+# print(f"{amount} INR is equal to {amount * float(currencyDict[currency])} {currency}")
